609498321cb07942b4c099be115baef862479266
Kiril Atanasoski <kiril@hubbyesim.com>
2024-11-05 16:06:37 +0100

    change the way of export

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-export * from './src/booking';
-export * from './src/package';
-export * from './src/partner';
-export * from './src/packageSpecifications';
-export * from './src/checkout';
-// export * from './src/traveler';
+export { bookingSchema, type BookingSchema } from './src/booking';
+export { packageSchema, type PackageSchema } from './src/package';
+export { partnerSchema, type PartnerSchema } from './src/partner';
+export { packageSpecificationsSchema, type PackageSpecificationsSchema } from './src/packageSpecifications';
+export { checkoutSchema, type CheckoutSchema } from './src/checkout';
