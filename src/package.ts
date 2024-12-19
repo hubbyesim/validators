e083d928ced7362385d061e05a21febd65317882
Daniel Fortuyn <daniel@frtn.nl>
2024-12-19 15:19:48 +0100

    added some stuff

--- src/package.ts
+++ src/package.ts
@@ -6,6 +6,9 @@ export interface PackageSchema {
   iso: string;
   days: number;
   price: number;
+  partner_price: number;
+  partner: string;
+  hidden: boolean;
 }
 
 const destinationPattern = /^[A-Z]{2,3}$/;
@@ -15,7 +18,10 @@ export const packageSchema = Joi.object<PackageSchema>({
   size: Joi.string().required().pattern(sizePattern), // size in the form of "500MB" or "3GB" or "3.25 GB"
   iso: Joi.string().required().pattern(destinationPattern),
   days: Joi.number().required(),
+  partner: Joi.string().optional().default(null),
+  hidden: Joi.boolean().default(false), // Removed required() since default() implies optional
   price: Joi.number().required(),
+  partner_price: Joi.number().required(),
 })
   .label('Package')
   .options({ abortEarly: false, stripUnknown: true });
